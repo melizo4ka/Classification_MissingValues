0764d99e47b40495bab4b585267c78f0a7e53c87
melizo4ka <64137900+melizo4ka@users.noreply.github.com>
2022-06-25 01:30:11 +0200

    .

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.metrics import accuracy_score
 
 class Node:
     def __init__(self, feature_index=None, threshold=None, left=None, right=None,
-                 info_gain=None, value=None, probability=1):
+                 info_gain=None, value=None, probability=None):
         # for decision node
         self.feature_index = feature_index
         self.threshold = threshold
@@ -31,7 +31,7 @@ class DecisionTreeClassifier:
         # split until stopping conditions are met
         if num_samples >= self.min_samples_split:
             # find the best split
-            best_split = self.get_best_split(dataset, num_samples, num_features)
+            best_split, values_list = self.get_best_split(dataset, num_samples, num_features)
             # check if information gain is positive
             if best_split["info_gain"] > 0:
                 # recur left
@@ -47,78 +47,83 @@ class DecisionTreeClassifier:
         return Node(value=leaf_value)
 
     def get_best_split(self, dataset, num_samples, num_features):
-        # dictionary to store the best split
-        prob = 1.0
-        global dataset_left, curr_info_gain, dataset_right
         best_split = {}
+        values_list = []
         max_info_gain = -float("inf")
-        # loop over all the features
         for feature_index in range(num_features):
             feature_values = dataset[:, feature_index]
+            for poss_value in feature_values:
+                # FOUND NAN VALUE
+                if math.isnan(poss_value):
+                    # check for possible values of feature
+                    for check_poss_val in feature_values:
+                        if not math.isnan(check_poss_val):
+                            if len(values_list) == 0:
+                                values_list.append([feature_index, check_poss_val, 1])
+                            found = False
+                            for i in range(len(values_list)):
+                                if not found:
+                                    found = False
+                                    if values_list[i][0] == feature_index:
+                                        if values_list[i][1] == check_poss_val:
+                                            values_list[i][2] = values_list[i][2] + 1
+                                            found = True
+                                    if not found:
+                                        # feature_index, poss_value, frequency
+                                        values_list.append([feature_index, check_poss_val, 1])
+                                        found = True
+
+            # return the unique values of particular feature
             possible_thresholds = np.unique(feature_values)
             # loop over all the feature values present in the data
             for threshold in possible_thresholds:
+                # get current split
+                dataset_left, dataset_right, probability_left, probability_right = self.split(dataset, feature_index,
+                                                                                              threshold, values_list)
+                # check if children are not null
+                if len(dataset_left) > 0 and len(dataset_right) > 0:
+                    y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
 
-                # check for NaN values
-                if math.isnan(threshold):
-                    prob_dict = {}
-                    # check for possible values of feature
-                    for check_poss_val in possible_thresholds:
-                        if not math.isnan(check_poss_val):
-                            # iterate over dictionary to check if this value of the attribute was already found
-                            value_found = False
-                            for key in prob_dict:
-                                # value already was found so we increase counter by 1
-                                if key == check_poss_val:
-                                    prob_dict[key] = prob_dict[key] + 1
-                                    value_found = True
-                            # add the value to dictionary with counter 1
-                            if not value_found:
-                                prob_dict[check_poss_val] = 1
-                    for key in prob_dict:
-                        value_prob = np.random.uniform()
-                        cumulative_prob = 0
-                        for keys in prob_dict:
-                            # calculating probability of that value of threshold
-                            item_prob = prob_dict[keys] / len(possible_thresholds)  # calculating prob
-                            cumulative_prob = item_prob + cumulative_prob
-                            if value_prob <= cumulative_prob:
-                                prob = (item_prob * prob)
-                                threshold = keys
-                        dataset_left, dataset_right = self.split(dataset, feature_index, threshold)
-                        if len(dataset_left) > 0 and len(dataset_right) > 0:
-                            y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
-                            weight_l = prob_dict[key]
-                            weight_r = 1 - prob_dict[key]
-                            curr_info_gain = self.entropy(y) - \
-                                (weight_l * self.entropy(left_y) + weight_r * self.entropy(right_y))
-                        else:
-                            curr_info_gain = max_info_gain
-                    prob_dict.clear()
-                else:
-                    # get current split
-                    dataset_left, dataset_right = self.split(dataset, feature_index, threshold)
-                    if len(dataset_left) > 0 and len(dataset_right) > 0:
-                        y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
-                        # compute information gain
-                        curr_info_gain = self.information_gain(y, left_y, right_y)
-                    else:
-                        curr_info_gain = max_info_gain
-                # update the best split if needed
-                if curr_info_gain > max_info_gain:
-                    best_split["feature_index"] = feature_index
-                    best_split["threshold"] = threshold
-                    best_split["dataset_left"] = dataset_left
-                    best_split["dataset_right"] = dataset_right
-                    best_split["info_gain"] = curr_info_gain
-                    best_split["probability"] = prob
-                    max_info_gain = curr_info_gain
-        return best_split
+                    # compute information gain
+                    curr_info_gain = self.information_gain(y, left_y, right_y)
+                    if curr_info_gain > max_info_gain:
+                        best_split["feature_index"] = feature_index
+                        best_split["threshold"] = threshold
+                        best_split["dataset_left"] = dataset_left
+                        best_split["probability_dataset_left"] = 1
+                        best_split["dataset_right"] = dataset_right
+                        best_split["probability_dataset_right"] = 1
+                        best_split["info_gain"] = curr_info_gain
+                        max_info_gain = curr_info_gain
+        return best_split, values_list
 
-    def split(self, dataset, feature_index, threshold):
+    def split(self, dataset, feature_index, threshold, values_list):
+        values = {}
+        count = 0
+        probability_left = 1
+        probability_right = 1
         dataset_left = np.array([row for row in dataset if row[feature_index] <= threshold])
         dataset_right = np.array([row for row in dataset if row[feature_index] > threshold])
-        return dataset_left, dataset_right
+
+        for i in range(len(values_list)):
+            while values_list[i][0] == feature_index:
+                # total cases for feature
+                count = count + values_list[i][2]
+                value_of_feature = values_list[i][1]
+                values[value_of_feature] = values_list[i][2]  # frequency
+
+        for i in range(len(values_list)):
+            while values_list[i][0] == feature_index:
+                value_of_feature = values_list[i][1]
+                values[value_of_feature] = (values[value_of_feature]) / count  # probability
+
+        for i in range(len(values_list)):
+            while values_list[i][0] == feature_index:
+                for j in range(len(values)):
+                    if j <= threshold:
+                        probability_left = probability_left * values[value_of_feature]
+                probability_right = 1 - probability_left
+        return dataset_left, dataset_right, probability_left, probability_right
 
     def information_gain(self, parent, l_child, r_child):
         weight_l = len(l_child) / len(parent)
@@ -144,8 +149,10 @@ class DecisionTreeClassifier:
         if tree.value is not None:
             print(tree.value)
         else:
-            print("X_" + str(tree.feature_index), "<=", tree.threshold, "Info Gain: ", tree.info_gain)
+            print("X_" + str(tree.feature_index), "<=", tree.threshold, "Info Gain: ", round(tree.info_gain, 3))
+            print("%sleft:" % (indent), end="")
             self.print_tree(tree.left, indent + indent)
+            print("%sright:" % (indent), end="")
             self.print_tree(tree.right, indent + indent)
 
     def fit(self, X, Y):
@@ -170,8 +177,10 @@ def testing(X, y):
     X_train, X_test, Y_train, Y_test = train_test_split(X, y, random_state=0)
     classifier = DecisionTreeClassifier(min_samples_split=3)
     classifier.fit(X_train, Y_train)
+    # classifier.print_tree()
     Y_pred = classifier.predict(X_test)
     print("Accuracy without missing values:", round(accuracy_score(Y_test, Y_pred), 3))
+    print()
 
     # random attributes deleted
     prob = array('f', [0, 0.1, 0.2, 0.5])
@@ -186,19 +195,23 @@ def testing(X, y):
         # classifier.print_tree()
         Y_pred = classifier.predict(X_test)
         print("Accuracy with probability of missing values", round(p, 2), ":", round(accuracy_score(Y_test, Y_pred), 3))
+        print()
 
 
 if __name__ == '__main__':
     # Glass Identification - 10 att + target class
     print("GLASS dataset")
+    print()
     df_g = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/glass/glass.data",
                        header=None)
     # target in last column
     Xg = df_g.iloc[:, :-1].values
     yg = df_g.iloc[:, -1].values.reshape(-1, 1)
     testing(Xg, yg)
+
     # Wine - 13 att + target class
     print("WINE dataset")
+    print()
     df_w = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data",
                        header=None)
     # target in first column
@@ -208,9 +221,11 @@ if __name__ == '__main__':
 
     # Iris - 4 att + target class
     print("IRIS dataset")
+    print()
     df_i = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", skiprows=1,
                        header=None)
     # target in last column
     Xi = df_i.iloc[:, :-1].values
     yi = df_i.iloc[:, -1].values.reshape(-1, 1)
     testing(Xi, yi)
+
